7e47b12d1328a9304606ae69c6363523d8c63af1
Hiep <nguyen.nhu@sequencetechno.com>
2026-01-30 22:39:56 +0700

    Initial Commit For Project

---
+++ src/main/java/org/example/co2collect/Co2CollectApplication.java
@@ -0,0 +1,13 @@
+package org.example.co2collect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Co2CollectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Co2CollectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/co2collect/Co2CollectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.co2collect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Co2CollectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
